232d21ee04991969a991d251c1c09dcaaa42adbc
crowe828 <69489498+crowe828@users.noreply.github.com>
2021-05-20 15:09:28 -0400

    Initial Commit

---
+++ chatInterface/chatInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatInterfaceApp.swift
+//  chatInterface
+//
+//  Created by Christian Rowe on 5/20/21.
+//
+
+import SwiftUI
+
+@main
+struct chatInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
